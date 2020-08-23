aa2a8c1310a17269cd5a385d38e0c580481a6ef9
HolyFoxMan <holyfoxman@gmail.com>
2020-08-23 06:42:05 +0000

    fix example vargs.c

--- examples/variadic_arguments.c
+++ examples/variadic_arguments.c
@@ -23,18 +23,19 @@ int main()
     CBQueue_t queue;
     CBQ_QueueInit(&queue, CBQ_SI_TINY, CBQ_SM_STATIC, 0, 0);
 
-    // 1 method
+    /* 1 method */
     /* see static_arguments.c to understand */
     CBQ_PushStatic(&queue, PrintSum, 3, (CBQArg_t){.iVar = 1}, (CBQArg_t){.iVar = 2}, (CBQArg_t){.iVar = 3});
 
-    // 2 method
+    /* 2 method */
     CBQArg_t[] variadic_arg = {
         {.iVar = 7}, {.iVar = 8}, {.iVar = 4}, {.iVar = 6}, {.iVar = 5}
     };
 
-    CBQ_PushOnlyVP(&queue, PrintSum, 5, variadic_arg); // 30
+    CBQ_PushOnlyVP(&queue, PrintSum, 5, variadic_arg):
 
     CBQ_Exec(&queue, NULL); // 6
+    CBQ_Exec(&queue, NULL); // 30
 
     CBQ_QueueFree(&queue);
     return 0;
